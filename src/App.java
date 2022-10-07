f1510da00bd7c41a763a53be51afd78f78565761
JoaoLuizGH <115193439+JoaoLuizGH@users.noreply.github.com>
2022-10-07 00:19:26 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       //Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+
+        Scanner teclado = new Scanner(System.in);
+
+        double vlrhora, hrstrab, salario;
+  
+        System.out.println("Informe o valor da hora de serviço: ");
+        vlrhora = teclado.nextDouble();
+
+        System.out.println("Informe quantas horas trabalhou no mês: ");
+        hrstrab = teclado.nextDouble();
+  
+        salario =  hrstrab * vlrhora;
+  
+        teclado.close();
+  
+        System.out.println(" Você irá receber R$ : " + salario ); 
     }
 }
